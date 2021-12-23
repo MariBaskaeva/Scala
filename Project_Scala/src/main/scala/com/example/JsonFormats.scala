7c1b49a73d6a305814ec21011dff1a9a12a55b38
MariBaskaeva <mariab1@list.ru>
2021-12-23 13:48:11 +0300

    Project

---
+++ Project_Scala/src/main/scala/com/example/JsonFormats.scala
@@ -0,0 +1,15 @@
+package com.example
+
+import com.example.UserRegistry.ActionPerformed
+
+//#json-formats
+import spray.json.DefaultJsonProtocol
+
+object JsonFormats  {
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
